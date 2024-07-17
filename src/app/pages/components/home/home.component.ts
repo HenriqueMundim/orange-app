c4548f390d0db8642d34d4b50841b728a1705792
Henrique Matos <henriquemundimmatos@gmail.com>
2024-07-17 17:16:04 -0300

    feat: Backgound image

--- src/app/pages/components/home/home.component.ts
+++ src/app/pages/components/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public isLogin = true;
+  
+  public loginForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.min(1), Validators.max(25)]]
+  })
+  
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class PagesModule { }
