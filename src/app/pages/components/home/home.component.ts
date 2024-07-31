84d562040756b8d39b0adaf08d090df4bf36ed95
Henrique Matos <henriquemundimmatos@gmail.com>
2024-07-31 09:46:51 -0300

    feat: input error detect When user insert a invalid value in input, they change

--- src/app/pages/components/home/home.component.ts
+++ src/app/pages/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { catchError, EMPTY } from 'rxjs';
@@ -15,13 +15,23 @@ import { AlertService } from 'src/app/shared/services/alert.service';
 export class HomeComponent implements OnInit {
 
   public route: String = "";
-  public isError: Boolean = false;
-  
+
   public loginForm: FormGroup = this.formBuilder.group({
-    email: ['', [Validators.required, Validators.min(1), Validators.max(100), Validators.email]],
-    password: ['', [Validators.required, Validators.min(6), Validators.max(16)]]
+    email: ['',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: ['',
+      [
+        Validators.required,
+        Validators.min(6),
+        Validators.max(16)
+      ]
+    ]
   })
-  
+
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
@@ -35,11 +45,8 @@ export class HomeComponent implements OnInit {
 
   public formhandler() {
     if (this.route == "/login") {
-
-      if(!this.loginForm.controls["email"].valid) {
-        console.log(this.loginForm.controls["email"].errors)
-      } else {
-        this.login()
+      if(this.loginForm.valid) {
+        this.login();
       }
     }
   }
