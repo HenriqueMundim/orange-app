db61567493838324b4ea7f37bded6764d60da125
Henrique Matos <henriquemundimmatos@gmail.com>
2024-08-09 09:13:21 -0300

    Register page
    * feat: Login page structure
    Put the images and create a register form
    
    * feat: Register page style
    Styling all register page creating the scss file
    
    * feat: input validation
    
    * Register user
    Regiter user integrated with backend

---
+++ src/app/core/interfaces/Iinput-user.interface.ts
@@ -0,0 +1,6 @@
+export interface IinputUser {
+  name: String
+  lastName: String
+  email: String
+  password: String
+}
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
@@ -8,7 +9,8 @@ const routes: Routes = [
     component: LoginComponent
   },
   {
-    path: 'register'
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
