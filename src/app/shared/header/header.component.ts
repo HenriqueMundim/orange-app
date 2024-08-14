672ade79feed69e05762ebdbaa30266bc48fe737
Henrique Matos <henriquemundimmatos@gmail.com>
2024-08-14 11:28:45 -0300

    feat: Dropdown menu Dropdown menu to mobile screen

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild("dropdownMenu") dropdownMenu: ElementRef<HTMLElement> | undefined
+  @ViewChild("openMenu") openMenu: ElementRef<HTMLElement> | undefined
+
+  private isMenuOpen = false;
+  public viewSize: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.viewSize = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  private onResize(event: Event) {
+    const windowtarget = event.target as Window
+
+    this.viewSize = windowtarget.innerWidth;
+  }
+
+  @HostListener('click', ['$event'])
+  private closeMenu(event: Event) {
+    if (this.isMenuOpen == true && event.target != this.openMenu?.nativeElement) {
+      this.dropdownMenu!.nativeElement.style.display="none";
+    }
+  }
+
+  public dropdown() {
+    this.dropdownMenu!.nativeElement.style.display="block";
+    this.isMenuOpen = true;
   }
 
 }
