26e268d3cde3232804cda8d8177129ef9ddc3019
Henrique Matos <henriquemundimmatos@gmail.com>
2024-08-26 10:13:49 -0300

    feat: upload preject image Upload image in amazon s3 bucket service created and change modal component to show image preview when upload in input file

---
+++ src/app/core/services/aws/aws-s3.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.dev';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AwsS3Service {
+
+  constructor(private http: HttpClient) {}
+
+  public uploadFile(file: File, token: String) {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+
+    return this.http.get<{url: string}>(`${environment.url}/aws/getpresignedurl/upload`, {
+      headers,
+      params: {
+        objectKey: file.name
+      }
+    }).subscribe({
+      next: (respose) => {
+        const obj = respose
+        fetch(obj.url, {
+          method: 'PUT',
+          body: file
+        }).then(res => console.log(res)).catch(err => console.log(err))
+      }
+    })
+
+  }
+}
--- src/app/shared/modal-register-project/modal-register-project.component.ts
+++ src/app/shared/modal-register-project/modal-register-project.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { AwsS3Service } from 'src/app/core/services/aws/aws-s3.service';
 
 @Component({
   selector: 'app-modal-register-project',
@@ -10,15 +11,18 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 export class ModalRegisterProjectComponent implements OnInit {
 
   public registerProjectForm: FormGroup = this.formBuilder.group({
-    image: [''],
+    image: [null],
     title: [''],
     tags: [''],
     link: ['']
-  })
+  });
+
+  @ViewChild('previewProject') previewProject!: ElementRef<HTMLImageElement>;
 
   constructor(
     public bsModalRef: BsModalRef,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private awsS3Service: AwsS3Service
   ) { }
 
   ngOnInit(): void {
@@ -31,6 +35,16 @@ export class ModalRegisterProjectComponent implements OnInit {
     const inputElement = event.target as HTMLInputElement;
     const file = inputElement.files ? inputElement.files[0] : null;
     this.registerProjectForm.patchValue({ image: file});
+    this.readFile();
   }
 
+  public registerProject(): void {
+    this.awsS3Service.uploadFile(this.registerProjectForm.get("image")?.value, "")
+  }
+
+  private readFile(): void {
+    const reader = new FileReader();
+    reader.readAsDataURL(this.registerProjectForm.controls["image"].value)
+    reader.onload = event => this.previewProject.nativeElement.src = reader.result as string
+  }
 }
